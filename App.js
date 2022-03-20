4a6d9e92081b9f95aeb00dad5bc8b2b4e3dee374
Fatih Kaya <56218812+fatihkaya13@users.noreply.github.com>
2022-03-21 00:51:09 +0300

    add bottom tab icons

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import CreditScreen from "./screens/CreditScreen";
@@ -7,6 +7,7 @@ import HistoryScreen from "./screens/HistoryScreen";
 import MapStack from "./screens/MapStack";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import Icon from "react-native-vector-icons/Ionicons";
 
 export default function App() {
   const BottomTab = createBottomTabNavigator();
@@ -14,21 +15,61 @@ export default function App() {
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <BottomTab.Navigator>
+        <BottomTab.Navigator  initialRouteName="Map">
           <BottomTab.Screen
             name="Credit"
             component={CreditScreen}
-            options={{ headerShown: false }}
+            options={{
+              headerShown: false,
+              tabBarIcon: ({ focused }) => (
+                <Image
+                  source={require("./assets/money.png")}
+                  resizeMode="contain"
+                  style={{
+                    width: 25,
+                    height: 25,
+                    tintColor: focused ? "#8a2be2" : "#748c94",
+                  }}
+                />
+              ),
+            }}
+            tabBarIcon={{}}
           />
           <BottomTab.Screen
             name="Map"
             component={MapStack}
-            options={{ headerShown: false }}
+            options={{
+              headerShown: false,
+              tabBarIcon: ({ focused }) => (
+                <Image
+                  source={require("./assets/bottom-maps.png")}
+                  resizeMode="contain"
+                  style={{
+                    width: 25,
+                    height: 25,
+                    tintColor: focused ? "#8a2be2" : "#748c94",
+                  }}
+                />
+              ),
+            }}
           />
           <BottomTab.Screen
             name="History"
             component={HistoryScreen}
-            options={{ headerShown: false }}
+            options={{
+              headerShown: false,
+              tabBarIcon: ({ focused }) => (
+                <Image
+                  source={require("./assets/history.png")}
+                  resizeMode="contain"
+                  style={{
+                    width: 25,
+                    height: 25,
+                    tintColor: focused ? "#8a2be2" : "#748c94",
+                  }}
+                />
+              ),
+            }}
           />
         </BottomTab.Navigator>
       </NavigationContainer>
